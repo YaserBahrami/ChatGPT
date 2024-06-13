954d17fe18f579e5488688adf6c12235fc45924d
Yaser Bahrami <yaserbahrami@outlook.com>
2024-06-14 02:40:11 +0300

    Add Chat View Model

---
+++ GPT360/Presentation/ViewModels/ChatViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  ChatViewModel.swift
+//  GPT360
+//
+//  Created by Yaser on 13.06.2024.
+//
+
+import Foundation
+import Combine
+
+class ChatViewModel: ObservableObject {
+    @Published var messages: [Message] = []
+    @Published var isLoading: Bool = false
+    
+    private var cancellables = Set<AnyCancellable>()
+    private let chatUseCase: ChatUseCase
+    
+    init(chatUseCase: ChatUseCase) {
+        self.chatUseCase = chatUseCase
+    }
+    
+    func sendMessage(_ text: String) {
+        isLoading = true
+        chatUseCase.fetchResponse(for: text)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                
+                self.isLoading = false
+                
+                if case .failure(let error) = completion {
+                    let errorMessage = Message(text: "Error: \(error.localizedDescription)", isUser: false)
+                    self.messages.append(errorMessage)
+                }
+            }, receiveValue: { [weak self] responseText in
+                guard let self = self else { return }
+                self.messages.append(Message(text: responseText, isUser: false))
+            })
+            .store(in: &cancellables)
+    }
+    func sendMessage2(_ text: String) {
+        let userMessage = Message(text: text, isUser: true)
+        messages.append(userMessage)
+        isLoading = true
+        
+        chatUseCase.fetchResponse(for: text)
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { [weak self] completion in
+                guard let self = self else { return }
+                self.isLoading = false
+                if case .failure(let error) = completion {
+                    let errorMessage = Message(text: "Error: \(error.localizedDescription)", isUser: false)
+                    self.messages.append(errorMessage)
+                }
+            }, receiveValue: { [weak self] responseText in
+                guard let self = self else { return }
+                let botMessage = Message(text: responseText, isUser: false)
+                self.messages.append(botMessage)
+            })
+            .store(in: &cancellables)
+    }
+}
